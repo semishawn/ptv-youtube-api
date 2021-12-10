eed0f87f570a7c537e313733a6914d79a1720dd6
semishawn <45698497+semishawn@users.noreply.github.com>
2021-12-10 10:33:50 -0500

    file redistribution

---
+++ api/read.php
@@ -0,0 +1,15 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+
+// Read contents of JSON files as strings
+$recentUploads = file_get_contents("data/recent-uploads.json");
+$profilesPlaylist = file_get_contents("data/profiles-playlist.json");
+
+// Encode both objects in array, encode into true JSON
+$data = json_encode(array($recentUploads, $profilesPlaylist));
+
+// Return encoded data
+echo $data;
+?>
--- api/write.php
+++ api/write.php
@@ -1,12 +1,13 @@
 <?php
-
 // General variables
 $apiKey = "AIzaSyC9x_rqvKIsbac47mpjcEnPUZ6K2W6SMKc";
 $channelId = "UC7PBoxHyuZp1EC2qzgLK71Q";
 
+
+
 // Generate API call for last 100 uploads
 $maxResults = "100";
-$recentUploads = preg_replace('/\s/', '', "
+$recentUploads = preg_replace('/\s/', "", "
 	https://www.googleapis.com/youtube/v3/search?
 	key=$apiKey
 	&channelId=$channelId
@@ -17,7 +18,7 @@ $recentUploads = preg_replace('/\s/', '', "
 
 // Copy API JSON data to file
 $uploadsJSON = file_get_contents($recentUploads);
-$uploadsFile = fopen('recent-uploads.json', 'w');
+$uploadsFile = fopen("data/recent-uploads.json", "w");
 fwrite($uploadsFile, $uploadsJSON);
 fclose($uploadsFile);
 
@@ -25,7 +26,7 @@ fclose($uploadsFile);
 
 // Generate API call for Parkland Profiles playlist
 $playlistId = "PLcTnBmAWjzjRzM5g6vvFbd3Mle6WTMNgn";
-$profilesPlaylist = preg_replace('/\s/', '', "
+$profilesPlaylist = preg_replace('/\s/', "", "
 	https://www.googleapis.com/youtube/v3/playlistItems?
 	&key=$apiKey
 	&part=snippet,id
@@ -34,8 +35,7 @@ $profilesPlaylist = preg_replace('/\s/', '', "
 
 // Copy API JSON data to file
 $playlistJSON = file_get_contents($profilesPlaylist);
-$playlistFile = fopen('profiles-playlist.json', 'w');
+$playlistFile = fopen("data/profiles-playlist.json", "w");
 fwrite($playlistFile, $playlistJSON);
 fclose($playlistFile);
-
 ?>
