d76b8287c1bbbde94af3a1a7e85f2cafafc6c49b
semishawn <45698497+semishawn@users.noreply.github.com>
2021-12-16 18:49:48 -0500

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,11 @@
+-- Schema to create table for recent uploads
+CREATE TABLE recent_uploads (
+	title TEXT NOT NULL,
+	id TEXT NOT NULL
+);
+
+-- Schema to create table for Parkland Profiles playlist
+CREATE TABLE profiles_playlist (
+	title TEXT NOT NULL,
+	id TEXT NOT NULL
+);
